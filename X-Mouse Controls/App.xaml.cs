b8aadf46e9b96809169eae8883abe06da6d8f82c
Joel Purra <mig@joelpurra.se>
2018-09-21 18:12:12 +0200

    Add file header

--- X-Mouse Controls/App.xaml.cs
+++ X-Mouse Controls/App.xaml.cs
@@ -1,3 +1,14 @@
+// <copyright file="App.xaml.cs" company="Joel Purra">
+// X-Mouse Controls by Joel Purra
+// Copyright © 2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018.
+// All rights reserved. Released under GNU General Public License version 3.0 (GPL-3.0).
+//
+// - https://joelpurra.com/projects/X-Mouse_Controls/
+// - https://github.com/joelpurra/xmouse-controls
+// - https://joelpurra.com/
+// - https://www.gnu.org/licenses/
+// </copyright>
+
 namespace XMouseControls
 {
     using System.Windows;
